aaf67789744b61cf8bfcddc856d99a40812f1ebc
dengolden <antoniomarquez1593@gmail.com>
2023-10-11 00:01:04 +0800

    initial commit

---
+++ app/src/main/java/com/example/weatherapp/Utils.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp
+
+class Utils {
+    companion object{
+
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = "ee4d7fbec0c8c3e4fc38996738b6f671"
+         const val PERMISSION_REQUEST_CODE = 123
+
+         const val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
+
+
+
+
+
+    }
+}
